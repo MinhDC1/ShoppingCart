d6557891d4f0ba79b95724be0dbb76285f16180b
minhdc1 <duongcongminh@gmail.com>
2022-10-18 08:25:39 -0400

    update files

--- src/main/client/src/app/app.module.ts
+++ src/main/client/src/app/app.module.ts
@@ -24,7 +24,6 @@ import { NzTableModule } from 'ng-zorro-antd/table';
 import { HeaderComponent } from './header/header.component';
 import { ProductsComponent } from './products/products.component';
 import { CartComponent } from './cart/cart.component';
-import { OrdersComponent } from './orders/orders.component';
 import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
@@ -35,7 +34,6 @@ import { CheckoutComponent } from './checkout/checkout.component';
     HeaderComponent,
     ProductsComponent,
     CartComponent,
-    OrdersComponent,
     CheckoutComponent
   ],
   imports: [
